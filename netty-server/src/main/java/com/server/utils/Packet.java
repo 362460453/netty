771bf8503cbc2a959f7d1c0e036426503fa34958
yanlin <362460453@qq.com>
2019-11-16 16:31:43 +0800

    提交

--- netty-server/src/main/java/com/server/utils/Packet.java
+++ netty-server/src/main/java/com/server/utils/Packet.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class Packet {
 
-    private Integer head = 42409;//同步头
+    private final Integer head = 42409;//同步头
     private Integer type;//类型
     private Integer length;//长度
     private Byte[] data;//数据包
